02c66d89ba3f28e9cfd0250ca49f518dbb22c9dd
estherhitos <esther.hitos@iescampanillas.com>
2021-10-26 19:53:11 +0200

    actualizacion

---
+++ TrabajoEnClase/PruebaWhile.java
@@ -0,0 +1,45 @@
+/*
+ * PruebaWhile.java
+ * 
+ * Copyright 2021 esther <esther@esther-VirtualBox>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+public class PruebaWhile {
+	public static void main(String[] args) {
+		System.out.println("Por favor, vaya introduciendo números y pulsando INTRO.");
+		System.out.println("Para terminar, introduzca un número negativo.");
+		
+		int numeroIntroducido = 0;
+		int cuentaNumeros = 0;
+		int suma = 0;
+		
+		while (numeroIntroducido >= 0) {
+			numeroIntroducido = Integer.parseInt(System.console().readLine());
+			if (numeroIntroducido >= 0){//evita que cuente negativos en el Bucle
+				cuentaNumeros++; // Incrementa en uno la variable
+				suma += numeroIntroducido; // Equivale a suma = suma + NumeroIntroducido
+			}
+		}
+		System.out.println("Has introducido " + cuentaNumeros  + " números positivos.");
+		System.out.println("La suma total de ellos es " + suma) ;
+	}
+}
+
