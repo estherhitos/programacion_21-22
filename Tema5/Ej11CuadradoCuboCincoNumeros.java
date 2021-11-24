d640721850230d55dbdd30b1f9831bd36bb29708
estherhitos <esther.hitos@iescampanillas.com>
2021-11-24 12:40:12 +0100

    actualizacion

--- Tema5/Ej11CuadradoCuboCincoNumeros.java
+++ Tema5/Ej11CuadradoCuboCincoNumeros.java
@@ -15,6 +15,28 @@ public class Ej11CuadradoCuboCincoNumeros {
 	
 	public static void main (String[] args) {
 		
+		int numeroIntroducido;
+		
+		System.out.println("Programa que muestra el cuadrado y el cubo de los 5 primeros números introducidos");
+		System.out.print("Porfabor introduzca minimo 5 numeros:");
+		numeroIntroducido=Integer.parseInt(System.console().readLine());
+		
+		for ( int i=numeroIntroducido; i<numeroIntroducido+5;i++){
+			System.out.printf("%4d %6d %8d\n", i, i * i, i * i * i);
+			}
+			
+//esta forma es con graficos y sin formatear e iniciando a uno 			
+		int cont=0, num1=1;
+
+				System.out.print("ingrese un numero:  ");
+
+					num1=Integer.parseInt(System.console().readLine());
+				System.out.println("numero-----cuadrado-------cubo");
+				System.out.println("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
+				for (cont=num1; cont<=num1+5; cont++){
+					
+				
+				System.out.println(" "+cont+" →→→→→→→→→→ "+cont*cont+" →→→→→→→→→ "+cont*cont*cont);}
 	}
 }
 
