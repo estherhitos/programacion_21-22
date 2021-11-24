2338bffcd74b0246daaa8dd92a0a810b493b6357
estherhitos <esther.hitos@iescampanillas.com>
2021-11-24 12:21:20 +0100

    actualizacion

---
+++ Tema5/Ej11CuadradoCuboCincoNumeros.java
@@ -0,0 +1,20 @@
+/*
+ * Ej11CuadradoCuboCincoNumeros.java
+ * 
+Escribe un programa que muestre en tres columnas, el cuadrado y el cubo de
+los 5 primeros números enteros a partir de uno que se introduce por teclado.
+ * 
+ * <= menor o igual que
+ * >= mayor o igual que
+ * 
+ * @Authora: Esther Hitos Garcia  
+ */
+
+
+public class Ej11CuadradoCuboCincoNumeros {
+	
+	public static void main (String[] args) {
+		
+	}
+}
+
